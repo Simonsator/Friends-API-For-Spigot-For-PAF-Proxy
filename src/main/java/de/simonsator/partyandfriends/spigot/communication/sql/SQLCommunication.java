dd381f04ead8c0b287d4f9bab72674f8f8c99a17
Simonsator <00pflaume@gmail.com>
2022-12-24 15:17:24 +0100

    Corrected error message

--- src/main/java/de/simonsator/partyandfriends/spigot/communication/sql/SQLCommunication.java
+++ src/main/java/de/simonsator/partyandfriends/spigot/communication/sql/SQLCommunication.java
@@ -98,8 +98,8 @@ public abstract class SQLCommunication {
 			return DriverManager.getConnection(URL, properties);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-			new ErrorReporter("§cParty and Friends was either not able to connect to the MySQL database or to login into the MySQL database. " +
-					"Please correct your MySQL data in the config.yml. If you need further help contact Simonsator via Discord (@Simonsator#5834), PM him (https://www.spigotmc.org/conversations/add?to=simonsator ) or write an email to him (support@simonsator.de). Please don't forget to send him the latets.log file. Also please don't write a bad review without giving him 24 hours time to fix the problem.");
+			new ErrorReporter("§cFriends API for Spigot for Party And Friends for Proxy was either not able to connect to the MySQL database or to login into the MySQL database. " +
+					"Please correct your MySQL data in the config.yml. If you need further help contact Simonsator via Discord (@Simonsator#5834), PM him (https://www.spigotmc.org/conversations/add?to=simonsator ) or write an email to him (support@simonsator.de). Please don't forget to send him the latest.log file. Also please don't write a bad review without giving him 24 hours time to fix the problem.");
 		}
 		return null;
 	}
