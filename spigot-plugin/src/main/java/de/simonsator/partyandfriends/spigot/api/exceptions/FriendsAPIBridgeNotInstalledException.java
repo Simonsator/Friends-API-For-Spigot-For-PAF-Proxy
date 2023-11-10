a6146ccaad0f0af4d000bee0586742db8dc3fb1d
Simonsator <00pflaume@gmail.com>
2023-11-10 19:45:32 +0100

    Better error message

--- spigot-plugin/src/main/java/de/simonsator/partyandfriends/spigot/api/exceptions/FriendsAPIBridgeNotInstalledException.java
+++ spigot-plugin/src/main/java/de/simonsator/partyandfriends/spigot/api/exceptions/FriendsAPIBridgeNotInstalledException.java
@@ -2,6 +2,6 @@ package de.simonsator.partyandfriends.spigot.api.exceptions;
 
 public class FriendsAPIBridgeNotInstalledException extends Exception {
 	public FriendsAPIBridgeNotInstalledException() {
-		super("The plugin needs to be installed on the proxy.");
+		super("§cTo use the %friendsapi_onlinefriendcount% placeholder you have to have this plugin installed on the spigot server and the bungeecord/velocity server. If you need further help contact Simonsator via Discord (@Simonsator#5834), PM him (https://www.spigotmc.org/conversations/add?to=simonsator ) or write an email to him (support@simonsator.de). Please don't forget to send him the latest.log file. Also please don't write a bad review without giving him 24 hours time to fix the problem.");
 	}
 }
